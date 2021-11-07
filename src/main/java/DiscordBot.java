fc3380e5986e0e7aa709e6915948b5cf667628d1
kaiffeetasse <qyp@web.de>
2021-11-07 20:18:00 +0100

    add .env support

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,16 +1,18 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
 import javax.security.auth.login.LoginException;
 
 public class DiscordBot extends ListenerAdapter {
 
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String DISCORD_BOT_TOKEN = dotenv.get("DISCORD_BOT_TOKEN");
 
     public static void main(String[] args) throws LoginException {
-        String token = "";
-
         MessageListener messageListener = new MessageListener();
 
-        JDABuilder.createDefault(token)
+        JDABuilder.createDefault(DISCORD_BOT_TOKEN)
                 .addEventListeners(messageListener)
                 .build();
     }
