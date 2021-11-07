91ecca97ff2759d1c7c79b8a24103e545f082ef7
kaiffeetasse <kaiffeetasse>
2021-11-07 19:57:20 +0100

    add discord bot

--- src/main/java/Vorlesung.java
+++ src/main/java/Vorlesung.java
@@ -1,6 +1,7 @@
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -15,5 +16,29 @@ public class Vorlesung {
     private String teacher;
     private String room;
     private String remark;
+    
+    @Override
+    public String toString(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm");
+
+        StringBuilder reply = new StringBuilder();
+
+        reply.append(this.getSubject())
+                .append(" ")
+                .append(this.getRemark())
+                .append("\nProf: ")
+                .append(this.getTeacher())
+                .append("\nRaum: ")
+                .append(this.getRoom())
+                .append("\nZeit: ")
+                .append(dateFormat.format(this.getStartTime()))
+                .append(" von ")
+                .append(timeFormat.format(this.getStartTime()))
+                .append(" bis ")
+                .append(timeFormat.format(this.getEndTime()));
+
+        return reply.toString();
+    }
 
 }
