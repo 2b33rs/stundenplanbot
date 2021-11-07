a2e7b0b87d75895b7c7dafee8938fd6487263d57
kaiffeetasse <qyp@web.de>
2021-11-07 22:52:48 +0100

    fix build

--- src/main/java/de/pils/bot/DiscordBot.java
+++ src/main/java/de/pils/bot/DiscordBot.java
@@ -4,11 +4,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
 import javax.security.auth.login.LoginException;
 
-public class DiscordBot extends ListenerAdapter {
+public class DiscordBot {
 
     private static final Dotenv dotenv = Dotenv.load();
     private static final String DISCORD_BOT_TOKEN = dotenv.get("DISCORD_BOT_TOKEN");
